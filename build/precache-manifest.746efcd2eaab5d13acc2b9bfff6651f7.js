254af9f126916b1970bd5d522e9570991a03157f
Amanat <amnaatmurzaliev@gmail.com>
2019-10-29 01:22:32 +0600

    heroku final configuration

---
+++ build/precache-manifest.746efcd2eaab5d13acc2b9bfff6651f7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d28530a7b69b6c26fab2425ce9e3fc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1a6438d4a7a486099e0",
+    "url": "/static/js/2.91bba52d.chunk.js"
+  },
+  {
+    "revision": "048bc129f03ac67b47d6",
+    "url": "/static/js/main.b30cfa20.chunk.js"
+  },
+  {
+    "revision": "023857dc4c6ceebfad34",
+    "url": "/static/js/runtime-main.030f8fb7.js"
+  },
+  {
+    "revision": "8cda0158c97ae863a7aacc1124d73013",
+    "url": "/static/media/facebook-icon.8cda0158.svg"
+  },
+  {
+    "revision": "8d124e45b92c45fbc839c00c6cf42588",
+    "url": "/static/media/google-icon.8d124e45.svg"
+  },
+  {
+    "revision": "fe84ffc3c95b6bc4b0559b6c55bf2b65",
+    "url": "/static/media/logo.fe84ffc3.svg"
+  },
+  {
+    "revision": "dc4c3d952c9c778a6099b272f698cd40",
+    "url": "/static/media/refresh-icon.dc4c3d95.svg"
+  },
+  {
+    "revision": "14cf3f18a34d1d18d66cbaa65b9a8ede",
+    "url": "/static/media/survapp-bg1.14cf3f18.jpg"
+  }
+]);
