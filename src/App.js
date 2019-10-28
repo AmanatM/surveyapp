38f435a8bb1245b8cffddda21a5348c35e2dc6bd
Amanat <amnaatmurzaliev@gmail.com>
2019-10-28 09:46:36 +0600

    PWA CHECK

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import GlobalStyle from './GlobalStyle' 
+
+import Loginform from './components/LoginForm'
+
+const Container = styled.div`
+  width: 90%;
+  max-width: 1280px;
+  margin: 0 auto;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Published to heroku server
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <GlobalStyle/>
+        <Container>
+          Amanat
+          <Loginform/>
+        </Container>
+      </div>
   );
 }
 
---
+++ src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoginForm = () => {
+
+    return (
+        <form className="loginForm">
+            <input type="text" placeholder="Username"/>
+        </form>
+    )
+}
+
+export default LoginForm
