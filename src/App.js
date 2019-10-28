2ae0332fbf42eb48f58e6b0a9dc789a912e49e0d
Amanat <amnaatmurzaliev@gmail.com>
2019-10-28 06:56:11 +0600

    Test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> Published to herok
+          Edit <code>src/App.js</code> Published to heroku server
         </p>
         <a
           className="App-link"
