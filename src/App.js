d24e28807ad1e075eef332d6f74870195e79b8e2
Amanat <amnaatmurzaliev@gmail.com>
2019-10-28 06:51:34 +0600

    Heroku deploy

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> test change
+          Edit <code>src/App.js</code> Published to heroku
         </p>
         <a
           className="App-link"
